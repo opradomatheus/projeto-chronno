5a5b872e54ff88a5477ab22252fb69387b313a71
Matheus Prado <tm.ferreira.prado@gmail.com>
2022-04-13 19:58:08 -0300

    update-css, js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 /* 
 
-Quando clicarmos no Pokémon desejado ira mostrar o card do mesmo e esconder o anterior.
+Quando clicarmos no personagem desejado ira mostrar o card do mesmo e esconder o anterior.
 
 
 Vamos trabalhar com dois elementos 
@@ -10,18 +10,18 @@ Vamos trabalhar com dois elementos
 
 Precisaremos criar duas variavéis no JS para trabalhar com os elementos da tela.
 
-Vamos utilizar o evento de clique feito de usuário  na listagem dos pokémon
+Vamos utilizar o evento de clique feito de usuário  na listagem dos personagens
 
 - Remover a classe aberto do card que estiver aberto
-- Ao clicar em um pokémon da listagem pegamos o id desse pokémon para saber qual o cartão mostrar.
-- Remover a classe ativa que marca o pokémon selecionado.
+- Ao clicar em um personagem da listagem pegamos o id do mesmo para saber qual o cartão mostrar.
+- Remover a classe ativa que marca o personagem selecionado.
 
 */
 
 //Variáveis que iremos utilizar para alterar os elementos da página.
 
 const listaSelecaoPersonagens = 
-document.querySelectorAll('.pokemon')
+document.querySelectorAll('.personagem')
 
 const Card = 
 document.querySelectorAll('.cartao-personagem')
